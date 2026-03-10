762a2bc31bc3ffc0e49cfe30edf93d9b207cb55e
ky3ow <vova2341591@gmail.com>
2026-03-10 18:22:13 +0000

    terraform fmt

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-lab3-havryliuk-volodymyr-2"
-    key = "env/dev/var-02.tfstate"
-    region = "eu-central-1"
-    encrypt = true
+    bucket       = "tf-state-lab3-havryliuk-volodymyr-2"
+    key          = "env/dev/var-02.tfstate"
+    region       = "eu-central-1"
+    encrypt      = true
     use_lockfile = true
   }
 }
